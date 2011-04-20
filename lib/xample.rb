7609508ecd170b088d3c59ef2581f6979e7aa5e8
Ola Bini <ola.bini@gmail.com>
2011-04-20 11:21:43 -0500

    Activate specific version of the libraries in order to not get trouble

--- lib/xample.rb
+++ lib/xample.rb
@@ -1,7 +1,12 @@
 
 require 'rubygems'
+
+gem 'ruby2ruby', '1.1.9'
+gem 'ParseTree', '2.2.0'
+
 require 'parse_tree'
 require 'ruby2ruby'
+
 require 'xample/exceptions'
 require 'xample/text'
 require 'xample/utils'
